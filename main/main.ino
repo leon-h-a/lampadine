697aa42e32c8deb5124554fffd2376c067e45597
leon-ha <lekileo@gmail.com>
2023-11-27 19:57:14 +0100

    feat(main): analog out ramp-up/down

--- main/main.ino
+++ main/main.ino
@@ -1,37 +1,31 @@
-/*
-  Blink
+int analog_out = 0;
+int up_flag = 1;
+int down_flag = 0;
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(0, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
+  analogWrite(0, analog_out);
+
+  if (analog_out >= 250) {
+    up_flag = 0;
+    down_flag = 1;
+  }
+  if (analog_out <= 10) {
+    up_flag = 1;
+    down_flag = 0;
+  }
+
+  if (up_flag == 1) {
+    analog_out += 10;
+  }
+  else {
+    analog_out -= 10;
+  }
+  
+  delay(50);
 }
